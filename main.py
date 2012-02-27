24a83ccd267845f79e1b62b20a89a7e2b92436c3
Brett Kaplan <darklink259@gmail.com>
2012-02-27 18:41:08 -0500

    stuff

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 
 from __future__ import division
 import pygame, sys, random
+from miniGame import MiniGame
 
 random.seed()
 #Global Variables
@@ -17,29 +18,39 @@ class Game(object):
         self.screen = pygame.display.set_mode(screenSize)
         pygame.display.set_caption("Giga-Bright presents:")
         self.clock = pygame.time.Clock()
+        self.miniGame = False
         #lots of other stuff will be needed, of course
         
     def process_events(self):
         """Process the event queue, take in player input."""
         #lots of other stuff will be needed, of course
         run = True
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                run = False
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_ESCAPE:
+        if self.miniGame:
+            miniGame.process_events(run)
+        else:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
                     run = False
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_ESCAPE:
+                        run = False
         return run
         
     def update(self):
         """Update the game objects."""
         #call update functions for all objects
-        pass
+        if self.miniGame:
+            miniGame.process_events()
+        else:
+            pass
         
     def draw(self):
         """Draw the game objects."""
         #call draw functions for all objects
-        pass
+        if self.miniGame:
+            self.miniGame.draw(self.screen)
+        else:
+            pass
         
 def main():
     g = Game()
