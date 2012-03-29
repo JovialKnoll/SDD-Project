7123de4f6cc2ec4c9653a33a802c4ad818788f39
Dave Strohl <strohd@rpi.edu>
2012-03-29 14:25:41 -0400

    Start of GUI

--- main.py
+++ main.py
@@ -10,8 +10,66 @@ random.seed()
 screenSize = (800, 600)
 fpsLimit = 30
 
+
+#Main Menu Player Avatar
+#Probably should make this inherit from the actor class...
+class Avatar(object):
+    
+    __velocity = (200, 200)
+    __res = (50,50)
+    
+    def __init__(self, pos):
+        """Makes an avatar for menu selection."""
+        self.rect = pygame.Rect((0,0), self.__res)
+        #self.surf = pygame.Surface((self.rect.width, self.rect.height))
+        self.setPos(pos)
+        self.state = {"left":False, "right":False, "up":False, "down":False}
+        
+        #Alter sprite pathing and use subsurfaces when sprite sheets are made
+        self.sprite = pygame.image.load("redSquare.png").convert_alpha()
+        
+    def setPos(self, pos):
+        self.rect.topleft = pos
+        
+    def getPos(self):
+        return self.rect.topleft
+    
+    def getRes(self):
+        return self.__res
+        
+    def update(self, deltaTime):
+        if self.state['left']:
+            self.setPos((self.rect.topleft[0] - self.__velocity[0] * deltaTime, self.rect.topleft[1]))
+        if self.state['up']:
+            self.setPos((self.rect.topleft[0],  self.rect.topleft[1] - self.__velocity[1] * deltaTime))
+        if self.state['right']:
+            self.setPos((self.rect.topleft[0] + self.__velocity[0] * deltaTime, self.rect.topleft[1]))
+        if self.state['down']:
+            self.setPos((self.rect.topleft[0],  self.rect.topleft[1] + self.__velocity[1] * deltaTime))
+        
+        
+    def draw(self, screen):
+        screen.blit(self.sprite, (self.rect.left, self.rect.top), self.sprite.get_rect())
+        pygame.display.flip()
+        
+
+        
+class loaderBox(object):
+
+    __res = (150, 50)
+    
+    def __init__(self, pos):
+        """Create a box to load a menu/game."""
+        self.rect = pygame.Rect((0,0), self.__res)
+        self.setPos(pos)
+        
+        
+
 #Game Object
 class Game(object):
+
+    __fps = 60 
+     
     def __init__(self):
         """Initialize the game."""
         pygame.init()
@@ -19,6 +77,8 @@ class Game(object):
         pygame.display.set_caption("Giga-Bright presents:")
         self.clock = pygame.time.Clock()
         self.miniGame = False
+        self.avatar = Avatar(((self.screen.get_width() / 2), self.screen.get_height() / 2))
+        self.avatar.setPos((self.avatar.getPos()[0] - self.avatar.getRes()[0] / 2, self.avatar.getPos()[1] - self.avatar.getRes()[1] / 2))
         #lots of other stuff will be needed, of course
         
     def process_events(self):
@@ -34,15 +94,37 @@ class Game(object):
                 if event.type == pygame.KEYDOWN:
                     if event.key == pygame.K_ESCAPE:
                         run = False
+                    if self.avatar:
+                        if event.key == pygame.K_RIGHT:
+                            self.avatar.state['right'] = True
+                        if event.key == pygame.K_LEFT:
+                            self.avatar.state['left'] = True
+                        if event.key == pygame.K_UP:
+                            self.avatar.state['up'] = True
+                        if event.key == pygame.K_DOWN:
+                            self.avatar.state['down'] = True
+                            
+                if event.type == pygame.KEYUP:
+                    if self.avatar:
+                        if event.key == pygame.K_RIGHT:
+                            self.avatar.state['right'] = False
+                        if event.key == pygame.K_LEFT:
+                            self.avatar.state['left'] = False
+                        if event.key == pygame.K_UP:
+                            self.avatar.state['up'] = False
+                        if event.key == pygame.K_DOWN:
+                            self.avatar.state['down'] = False
         return run
         
     def update(self):
         """Update the game objects."""
         #call update functions for all objects
+        delta_time = float(self.clock.tick(self.__fps)) / 1000
+        
         if self.miniGame:
             miniGame.update()
         else:
-            pass
+            self.avatar.update(delta_time)
         
     def draw(self):
         """Draw the game objects."""
@@ -50,7 +132,8 @@ class Game(object):
         if self.miniGame:
             self.miniGame.draw(self.screen)
         else:
-            pass
+            self.screen.fill(pygame.Color("black"))
+            self.avatar.draw(self.screen)
         
 def main():
     g = Game()
